666f5b770be23eb6d1fa13c4719684980bd7afbe
patagona <patagonahn@gmail.com>
2024-03-19 04:41:11 +0100

    code: add serial port prefix to log, fix endless retry bug

--- bms_comm/src/SurronCommunication/Communication/SurronCommunicationHandler.cs
+++ bms_comm/src/SurronCommunication/Communication/SurronCommunicationHandler.cs
@@ -12,22 +12,24 @@ namespace SurronCommunication.Communication
     public sealed class SurronCommunicationHandler : ISurronCommunicationHandler
     {
         private readonly ICommunication _communication;
+        private readonly string _logPrefix;
 
-        internal SurronCommunicationHandler(ICommunication communication)
+        internal SurronCommunicationHandler(ICommunication communication, string logPrefix)
         {
             _communication = communication ?? throw new ArgumentNullException(nameof(communication));
+            _logPrefix = logPrefix;
         }
 
         public static SurronCommunicationHandler FromSerialPort(string serialPort)
         {
-            return new SurronCommunicationHandler(new SerialCommunication(serialPort));
+            return new SurronCommunicationHandler(new SerialCommunication(serialPort), serialPort);
         }
 
         public byte[]? ReadRegister(ushort address, byte parameter, byte paramLength, CancellationToken cancellationToken)
         {
             var sendPacket = SurronDataPacket.Create(SurronCmd.ReadRequest, address, parameter, paramLength, null);
 
-            for (int sendRetryCounter = 1; ; sendRetryCounter++)
+            for (int sendRetryCounter = 0; sendRetryCounter < 3; sendRetryCounter++)
             {
                 cancellationToken.ThrowIfCancellationRequested();
 
@@ -38,6 +40,7 @@ namespace SurronCommunication.Communication
                 {
                     // 9600 baud 8N1 = ~960 bytes/s, so 200ms are enough for ~192 bytes.
                     var packet = ReceivePacket(200, cancellationToken);
+
                     if (packet != null)
                     {
                         if (packet.Address == address && packet.Parameter == parameter && packet.DataLength == paramLength)
@@ -45,9 +48,8 @@ namespace SurronCommunication.Communication
                         Debug.WriteLine($"Wrong Packet {packet}");
                         continue;
                     }
-                    if (sendRetryCounter >= 3)
-                        return null;
                     Debug.WriteLine("Timeout");
+                    continue;
                 }
                 catch (OperationCanceledException)
                 {
@@ -56,20 +58,22 @@ namespace SurronCommunication.Communication
                 catch (InvalidDataException dataEx)
                 {
                     Debug.WriteLine($"Invalid Data: {dataEx.Message}");
-                    Thread.Sleep(100); // can not be too high or else BMS goes back into standby (after ~3s)
                 }
                 catch (Exception ex)
                 {
                     Debug.WriteLine($"Unknown Exception: {ex}");
-                    Thread.Sleep(100); // can not be too high or else BMS goes back into standby (after ~3s)
                 }
+
+                Thread.Sleep(100); // can not be too high or else BMS goes back into standby (after ~3s)
             }
+
+            return null;
         }
 
         public void SendPacket(SurronDataPacket packet, CancellationToken token)
         {
             var toSend = packet.ToBytes();
-            Debug.WriteLine($">{HexUtils.BytesToHex(toSend)}");
+            Debug.WriteLine($"{_logPrefix}>{HexUtils.BytesToHex(toSend)}");
             _communication.Write(toSend, token);
         }
 
@@ -90,7 +94,7 @@ namespace SurronCommunication.Communication
                     return null;
                 bufferPos += restLength;
 
-                Debug.WriteLine($"<{HexUtils.BytesToHex(buffer.AsSpan(0, bufferPos).ToArray())}");
+                Debug.WriteLine($"{_logPrefix}<{HexUtils.BytesToHex(buffer.AsSpan(0, bufferPos).ToArray())}");
                 return SurronDataPacket.FromBytes(buffer.AsSpan(0, bufferPos));
             }
             catch (InvalidDataException)
